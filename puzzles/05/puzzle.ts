83b4b8c404d2b171d2bb9c5cc722da7292d43e99
Ian Hornik <yiliansource@gmail.com>
2022-12-05 11:05:53 +0100

    cleanup 05

--- puzzles/05/puzzle.ts
+++ puzzles/05/puzzle.ts
@@ -5,57 +5,21 @@ export default {
     expectedOutput: ["CMZ", "MCD"],
     solvers: [
         (input) => {
-            const rowCount = input.findIndex((l) => !l.trimStart().startsWith("["));
-            const columnLabels = input[rowCount].split(/\s+/).filter(Boolean);
-            const columnCount = parseInt(columnLabels[columnLabels.length - 1]);
-            const stacks: string[][] = [];
-            for (let i = 0; i < columnCount; i++) {
-                stacks.push([]);
-                for (let j = rowCount - 1; j >= 0; j--) {
-                    const cursor = i * 4;
-                    if (input[j][cursor] !== " ") {
-                        stacks[i].push(input[j][cursor + 1]);
-                    }
-                }
-            }
-
-            const instructions = input.slice(rowCount + 2).map((r) =>
-                Array.from(r.matchAll(/move (\d+) from (\d+) to (\d+)/g))[0]
-                    .slice(1, 4)
-                    .map(Number)
-            );
+            const [stacks, instructions] = parseInput(input);
 
             for (const [count, from, to] of instructions) {
-                const pop = stacks[from - 1].splice(stacks[from - 1].length - count, count).reverse();
-                stacks[to - 1].push(...pop);
+                const pop = stacks[from].splice(stacks[from].length - count, count).reverse();
+                stacks[to].push(...pop);
             }
 
             return stacks.map((s) => s[s.length - 1]).join("");
         },
         (input) => {
-            const rowCount = input.findIndex((l) => !l.trimStart().startsWith("["));
-            const columnLabels = input[rowCount].split(/\s+/).filter(Boolean);
-            const columnCount = parseInt(columnLabels[columnLabels.length - 1]);
-            const stacks: string[][] = [];
-            for (let i = 0; i < columnCount; i++) {
-                stacks.push([]);
-                for (let j = rowCount - 1; j >= 0; j--) {
-                    const cursor = i * 4;
-                    if (input[j][cursor] !== " ") {
-                        stacks[i].push(input[j][cursor + 1]);
-                    }
-                }
-            }
-
-            const instructions = input.slice(rowCount + 2).map((r) =>
-                Array.from(r.matchAll(/move (\d+) from (\d+) to (\d+)/g))[0]
-                    .slice(1, 4)
-                    .map(Number)
-            );
+            const [stacks, instructions] = parseInput(input);
 
             for (const [count, from, to] of instructions) {
-                const pop = stacks[from - 1].splice(stacks[from - 1].length - count, count);
-                stacks[to - 1].push(...pop);
+                const pop = stacks[from].splice(stacks[from].length - count, count);
+                stacks[to].push(...pop);
             }
 
             return stacks.map((s) => s[s.length - 1]).join("");
@@ -63,10 +27,29 @@ export default {
     ],
 } as PuzzleModel<string>;
 
-function partitionString(str: string, length: number, gap: number = 0): string[] {
-    const output: string[] = [];
-    for (let i = 0; i < str.length; i += length + gap) {
-        output.push(str.slice(i, i + length));
+function parseInput(input: string[]): [stacks: string[][], instructions: [number, number, number][]] {
+    const rowCount = input.findIndex((l) => !l.trimStart().startsWith("["));
+    const columnLabels = input[rowCount].split(/\s+/).filter(Boolean);
+    const columnCount = parseInt(columnLabels[columnLabels.length - 1]);
+    const stacks: string[][] = [];
+    for (let i = 0; i < columnCount; i++) {
+        stacks.push([]);
+        for (let j = rowCount - 1; j >= 0; j--) {
+            const cursor = i * 4;
+            if (input[j][cursor] !== " ") {
+                stacks[i].push(input[j][cursor + 1]);
+            }
+        }
     }
-    return output;
+
+    const instructions = input
+        .slice(rowCount + 2)
+        .map((r) =>
+            Array.from(r.matchAll(/move (\d+) from (\d+) to (\d+)/g))[0]
+                .slice(1, 4)
+                .map(Number)
+        )
+        .map(([count, from, to]) => [count, from - 1, to - 1] as [number, number, number]);
+
+    return [stacks, instructions];
 }
